e5fb8395c2bd8bc5bc507e41d3a1e0a8639830dc
awana01 <automationsingh.1983@gmail.com>
2024-05-15 12:37:15 +0530

    add new test file

---
+++ TestExample/TestRepeat1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectExample1.TestExample
+{
+
+    [TestFixture]
+    public class TestRepeat1:TestBase1
+    {
+
+        [SetUp]
+        public void InitTest()
+        {
+
+        }
+
+        [TearDown]
+        public void Cleanup() 
+        { 
+        }
+
+        [Test]
+        [Repeat(2)]
+        public void Test1()
+        {
+            Console.WriteLine("Test Class");
+        }
+    
+    }
+}
--- obj/Debug/net8.0/TestProjectExample1.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProjectExample1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjectExample1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adab0fc1005c12586ba00cd3ae2a4ec73109dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjectExample1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjectExample1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
