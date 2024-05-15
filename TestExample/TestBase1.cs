1adab0fc1005c12586ba00cd3ae2a4ec73109dec
awana01 <automationsingh.1983@gmail.com>
2024-05-15 11:15:00 +0530

    sample test project

---
+++ TestExample/TestBase1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectExample1.TestExample
+{
+    //[SetUpFixture]
+    public class TestBase1
+    {
+
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            Console.WriteLine($"Executing Global OneTimeSetUp: {TestContext.CurrentContext.Test.FullName} For: {TestContext.CurrentContext.Test.Name}");
+        }
+
+        [SetUp]
+        public void InitTest()
+        {
+            Console.WriteLine($"Executing Global SetUp: {TestContext.CurrentContext.Test.FullName} For: {TestContext.CurrentContext.Test.Name}");
+        }
+
+        [TearDown]
+        public void CleanUpTest()
+        {
+            Console.WriteLine($"Executing Global TearDown: {TestContext.CurrentContext.Test.FullName} For: {TestContext.CurrentContext.Test.Name}");
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTearDown() 
+        {
+            Console.WriteLine($"Executing Global OneTimeTearDown: {TestContext.CurrentContext.Test.FullName} For: {TestContext.CurrentContext.Test.Name}");
+        }
+        
+    }
+}
---
+++ TestExample/TestEx1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectExample1.TestExample
+{
+
+    [TestFixture]
+    public class TestEx1: TestBase1
+    {
+
+        [SetUp]
+        public void TestLevelSetUp()
+        {
+            Console.WriteLine($"Executing Test-Level: {TestContext.CurrentContext.Test.FullName} For: {TestContext.CurrentContext.Test.Name}");
+        }
+
+        [TearDown]
+        public void TestCleanUp()
+        {
+            Console.WriteLine($"Executing Test-Level: {TestContext.CurrentContext.Test.FullName} For: {TestContext.CurrentContext.Test.Name}");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine($"========= Test Case {TestContext.CurrentContext.Test.Name} Execution ========");
+            Console.WriteLine($"Executing TestCase: {TestContext.CurrentContext.Test.Name}");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Console.WriteLine($"========= Test Case {TestContext.CurrentContext.Test.Name} Execution ========");
+            Console.WriteLine($"Executing TestCase: {TestContext.CurrentContext.Test.Name}");
+        }
+
+    }
+}
